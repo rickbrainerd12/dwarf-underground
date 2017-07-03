f42f940a7f8af22ccf1d11ef19a55eaf90452ac3
Rick Brainerd <rickbrainerd12@gmail.com>
2017-07-03 12:18:25 -0400

    Split App into smaller components

---
+++ src/Article.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Article.css'
+
+class Header extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Header
